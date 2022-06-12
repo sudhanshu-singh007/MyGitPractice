36dbcc8401367f2d8f3f153db67ae5d51256d576
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-12 11:53:02 +0530

    my 3rd commit in test

---
+++ src/main/java/javaPractice/MyThirdClassInTest.java
@@ -0,0 +1,11 @@
+package javaPractice;
+
+public class MyThirdClassInTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my 3rd commit in test");
+
+	}
+
+}
