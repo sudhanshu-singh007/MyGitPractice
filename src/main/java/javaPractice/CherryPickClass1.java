efde84de4820e1e68c9b56ce5697d13e6195261b
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-12 12:37:14 +0530

    cherrypick 1

---
+++ src/main/java/javaPractice/CherryPickClass1.java
@@ -0,0 +1,11 @@
+package javaPractice;
+
+public class CherryPickClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("cherrypick 1");
+
+	}
+
+}
