e383ec5cfbb3db7f63a0b7d4d9ae2947c4310038
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-12 11:54:18 +0530

    my 3rd commit in master

---
+++ src/main/java/javaPractice/MyThirdClassInMaster.java
@@ -0,0 +1,11 @@
+package javaPractice;
+
+public class MyThirdClassInMaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my 3rd commit in master");
+
+	}
+
+}
