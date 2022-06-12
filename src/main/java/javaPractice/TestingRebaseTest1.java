237e14b46c32634c5c948463f2b4e6f15b7f1786
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-12 12:01:27 +0530

    testing rebase in test 1

---
+++ src/main/java/javaPractice/TestingRebaseTest1.java
@@ -0,0 +1,11 @@
+package javaPractice;
+
+public class TestingRebaseTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing rebase in test 1");
+
+	}
+
+}
