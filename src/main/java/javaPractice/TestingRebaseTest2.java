e1adbd95d8001a20de2f98c2479023569a046ba7
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-12 16:04:38 +0530

    Revert "testing revert"
    This reverts commit 672708e94a43deccae33a469ed9ae446b0d23e54
    I am sorry
    :wq
    :Wq

--- src/main/java/javaPractice/TestingRebaseTest2.java
+++ src/main/java/javaPractice/TestingRebaseTest2.java
@@ -5,7 +5,6 @@ public class TestingRebaseTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testing rebase in test 2");
-		System.out.println("testing rebase in test 2");
 
 	}
 
