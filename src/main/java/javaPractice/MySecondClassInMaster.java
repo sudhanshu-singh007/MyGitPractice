dda74477d5b0c6344031b5140036c896c3d0acd4
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-12 11:40:55 +0530

    my 2nd commit to master

---
+++ src/main/java/javaPractice/MySecondClassInMaster.java
@@ -0,0 +1,11 @@
+package javaPractice;
+
+public class MySecondClassInMaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my 2nd commit in master");
+
+	}
+
+}
