6be7525fae390c809865c08af3654bb2274ba7d2
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-12 11:49:46 +0530

    my first commit in test

---
+++ src/main/java/javaPractice/MyFirstClassInTest.java
@@ -0,0 +1,11 @@
+package javaPractice;
+
+public class MyFirstClassInTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first commit in test");
+
+	}
+
+}
